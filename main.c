bcea5d1a52656aa1064007ea95aaa0a2dec6d10b
polat <aydinpolat@post.com>
2021-05-03 00:58:04 +0300

    reading file and encrypting a word is done.

--- main.c
+++ main.c
@@ -1,35 +1,63 @@
 #include <stdio.h>
 
-int* hashEncrypt(int *i, int a,int k[]);
+int *hashEncrypt(int *i);
+void fileReadAndConvertHash(char filePath[], FILE *fp);
+
+int a = 0;
+int hashVeri[100];
+int hash2[100];
+int *hashed;
 
 int main() {
-    int a = 0;
-    int veri[100] = {'a','y','d','i','n'};
-    int hashVeri[100] ;
-    int *hashed = hashEncrypt(veri,a,hashVeri);
-    for (int i = 0; i < 5; ++i) {
-        printf("%c",hashed[i]);
-    }
+    int veri[100] = {'a', 'y', 'd', 'i', 'n'};
 
+    char filePath[500] = "C:\\Users\\polat\\CLionProjects\\algoritmaAnaliziProje\\kelimeler.txt";
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    fileReadAndConvertHash(filePath, fp);
 }
 
-int* hashEncrypt(int *i,int a,int k[]) {
-    if(i[a]){
+void fileReadAndConvertHash(char filePath[], FILE *fp) {
+    fp = fopen(filePath, "r");
+    if (fp == NULL) {
+        printf("Dosya boş yada hatalı");
+    }
+    int b = 0;
+    do {
+        hash2[b] = (int) fgetc(fp);
+        b++;
+    } while (hash2[b - 1] != -1);
+
+    hashed = hashEncrypt(hash2);
+    for (int i = 0; i < 5; ++i) {
+        printf("%c", hashed[i]);
+    }
+
+
+    fclose(fp);
+}
+
+int *hashEncrypt(int *i) {
+    if (i[a] != 10) {
         int deger = i[a];
-        if(deger >= 33 && deger < 80){
+        if (deger >= 33 && deger < 80) {
             deger = deger + 46;
             char charDonustur = (char) deger;
-            k[a] = charDonustur;
-            a++;
-        }if(deger >= 80 && deger <= 126){
-            deger = deger - 46;
-            char charDonustur = (char) deger;
-            k[a] = charDonustur;
+            hashVeri[a] = charDonustur;
             a++;
         }
-        hashEncrypt(i,a,k);
-    }else {
+        if (deger >= 80 && deger <= 126) {
+            deger = deger - 46;
+            char charDonustur = (char) deger;
+            hashVeri[a] = charDonustur;
+            a++;
+        }
+        hashEncrypt(i);
+    } else {
+        a = 0;
         printf("İşlem Başarılı\n\nhashed veri = ");
-        return k;
+        return hashVeri;
     }
 }
